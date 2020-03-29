8ae40d9588f072b14eea27969867489cd14c3776
yangyigit <1187161570@qq.com>
2020-03-29 15:28:12 +0800

    11

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: '我是大吉',
+    title: '我是大吉-isdaji',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
